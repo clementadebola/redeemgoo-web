04a68ebf5117216715d0e67b6e3e548036ea7ca9
clement adebola <clementadebola54@gmail.com>
2026-06-11 23:29:12 +0100

    new

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,22 +1,23 @@
+// lib/firebase.ts
 import { initializeApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDlivlPmam81bJPpFzgn771Ofw8iQq8yjw",
-  authDomain: "eggsy-df134.firebaseapp.com",
-  projectId: "eggsy-df134",
-  storageBucket: "eggsy-df134.appspot.com",
-  messagingSenderId: "350355807592",
-  appId: "1:350355807592:web:93517c887dbff140f30add"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Prevent duplicate initialization
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const app =
+  getApps().length === 0
+    ? initializeApp(firebaseConfig)
+    : getApps()[0];
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
 
 export default app;
